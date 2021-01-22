41a3abeacab393ebb7d19e3cdc4bdbc24a12df85
Soliyana Anno <solianademere@gmail.com>
2021-01-23 00:26:45 +0100

    Sai and Soliyana designs

--- app/src/main/java/com/example/bookings/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/bookings/ui/dashboard/DashboardFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,25 +21,41 @@ import com.example.bookings.R;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private Spinner hotelSpinner;
+    private EditText editTextDatearrival;
+    private EditText editTextDatedeparture;
+    private EditText editTextNumberguests;
+    private Button bookingroombutton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final Button b = root.findViewById(R.id.button);
-        final TextView tv,tv2,tv3,tv4,tv5,tv6,tv7;
+        final Button bookingroombutton = root.findViewById(R.id.bookingroombutton);
+
+/*        final TextView tv,tv2,tv3,tv4,tv5,tv6,tv7;
         tv=root.findViewById(R.id.textView);
         tv2=root.findViewById(R.id.textView2);
         tv3=root.findViewById(R.id.textView3);
         tv4=root.findViewById(R.id.textView4);
         tv5=root.findViewById(R.id.textView5);
         tv6=root.findViewById(R.id.textView6);
-        tv7=root.findViewById(R.id.textView7);
-        final EditText editText,editText1,editText2;
-        editText=root.findViewById(R.id.editTextNumber);
-        editText1=root.findViewById(R.id.editTextDate);
-        editText2=root.findViewById(R.id.editTextDate2);
+        tv7=root.findViewById(R.id.textView7);*/
+
+        final EditText editTextDatearrival,editTextDatedeparture,editTextNumberguests;
+
+        editTextDatearrival=root.findViewById(R.id.editTextDatearrival);
+        editTextDatedeparture=root.findViewById(R.id.editTextDatedeparture);
+        editTextNumberguests=root.findViewById(R.id.editTextNumberguests);
+
+        final Spinner hotelSpinner;
+        hotelSpinner=root.findViewById(R.id.hotelSpinner);
+
+       // String[] hotelBranches = {"Hotel Paris","Hotel Miami","Hotel Maldives","Hotel London"};
+      //  ArrayAdapter arrayAdapterH = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,hotelBranches);
+      //  hotelSpinner.setAdapter(arrayAdapterH);
+
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
--- app/src/main/java/com/example/bookings/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bookings/ui/home/HomeFragment.java
@@ -1,35 +1,60 @@
 package com.example.bookings.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+
 import com.example.bookings.R;
+import com.example.bookings.RegisterActivity;
+import com.example.bookings.ui.dashboard.DashboardFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button BooknowButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+
+        Button BooknowButton = root.findViewById(R.id.BooknowButton);
+        BooknowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                DashboardFragment dashboardFragment = new DashboardFragment();
+                FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment,dashboardFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
-            }
-        });
+            }*/
+
         return root;
+
+       // });
+
     }
+
 }
